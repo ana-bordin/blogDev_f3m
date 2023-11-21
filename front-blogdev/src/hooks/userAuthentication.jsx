e3c11df767f1046404d75a9e3763e350432aac30
Ana Bordin <118741376+AnnyaB@users.noreply.github.com>
2023-11-21 19:45:23 -0300

    userauthentication

--- front-blogdev/src/hooks/userAuthentication.jsx
+++ front-blogdev/src/hooks/userAuthentication.jsx
@@ -1,33 +1,33 @@
 import { db } from '../firebase/config';
-import {
+import{
     getAuth,
     createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
     updateProfile,
     signOut,
-} from 'firebase/auth'
+} from 'firebase/auth';
 import { useState, useEffect } from 'react'
 
-export const userAuthentication = () => {
+export const userAuthentication = () =>{
     const [error, setError] = useState(null)
     const [loading, setLoading] = useState(null)
     const [cancelled, setCancelled] = useState(false)
 
     const auth = getAuth()
 
-    function checkIfIsCancelled() {
-        if (cancelled) {
+    function checkIfIsCancelled(){
+        if(cancelled){
             return
         }
     }
 
-    async function createUser(data) {
+    async function createUser(data){
         checkIfIsCancelled()
-
+        
         setLoading(true)
         setError(null)
 
-        try {
+        try{
             const { user } = await createUserWithEmailAndPassword(
                 auth,
                 data.email,
@@ -41,34 +41,33 @@ export const userAuthentication = () => {
             setLoading(false)
 
             return user
-
-        } catch (error) {
+        }catch(error){
             console.error(error.message)
             console.table(typeof error.message)
 
             let systemErrorMessage
 
-            if (error.message.includes("Password")) {
+            if(error.message.includes("Password")){
                 systemErrorMessage = "A senha precisa conter pelo menos 6 caracteres"
-            } else if (error.message.includes("email-already")) {
-                systemErrorMessage = "E-mail ja cadastrado"
-            } else {
-                systemErrorMessage = "Ocorreu um erro, tente novamente mais tarde"
+            }else if(error.message.includes("email-already")){
+                systemErrorMessage = "E-mail já cadastrado"
+            }else{
+                systemErrorMessage = "Ocorreu um error, tente novamente mais tarde"
             }
-
+            
             setLoading(false)
             setError(systemErrorMessage)
         }
     }
-
+    
     useEffect(() => {
         return () => setCancelled(true)
     }, [])
-    
+
     return {
-        auth, 
-        createUser, 
-        error, 
+        auth,
+        createUser,
+        error,
         loading
     }
 }
